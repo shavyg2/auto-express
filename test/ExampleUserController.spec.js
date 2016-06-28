7b61d860ee52187706790ae9ec4a50d5ae3e3047
Shavauhn Gabay <shavyg2@gmail.com>
2016-06-28 07:19:59 -0400

    added example controller

---
+++ test/ExampleUserController.spec.js
@@ -0,0 +1,8 @@
+import chai from "chai";
+const should = chai.should();
+import Controller from "../Controller/ExampleUserController";
+
+describe("BaseController Test", function() {
+  let controller;
+
+})
