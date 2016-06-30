db9752b277b0a5f8483ba70a63884f5e657d3120
Shavauhn Gabay <shavauhng@i3inc.ca>
2016-06-30 06:03:56 -0400

    transfer index to index.example.js

---
+++ app/index.example.js
@@ -0,0 +1,13 @@
+"use strict";
+
+var _boot = require("./boot");
+
+/**************************
+
+  Do what ever you want
+  and then do then boot;
+  eg DatabaseLoading
+
+**************************/
+
+(0, _boot.Boot)();
---
+++ src/app/index.example.js
@@ -0,0 +1,16 @@
+import {Boot} from "./boot";
+
+
+
+
+/**************************
+
+  Do what ever you want
+  and then do then boot;
+  eg DatabaseLoading
+
+**************************/
+
+
+
+Boot();
