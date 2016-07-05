454ad4cbd1bb24325dbc3d3cc5234576f32a0d6b
Shavauhn Gabay <shavauhng@i3inc.ca>
2016-07-05 01:37:40 -0400

    change merging of config

--- app/config/default.js
+++ app/config/default.js
@@ -3,6 +3,7 @@
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
+exports.Merge = Merge;
 
 var _SwigRenderer = require("../../Renderer/SwigRenderer");
 
@@ -17,6 +18,41 @@ var _lodash = require("lodash");
 var _lodash2 = _interopRequireDefault(_lodash);
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+function Merge(dest, src) {
+  dest = dest || {};
+  src = src || {};
+
+  var dest_keys = _lodash2.default.keysIn(dest);
+  var src_keys = _lodash2.default.keysIn(src);
+  var _return = {};
+
+  var keys = _lodash2.default.concat(dest_keys, src_keys);
+  for (var i = 0, len = keys.length; i < len; i++) {
+    var key = keys[i],
+        value1 = dest[key],
+        value2 = src[key],
+        value = void 0;
+
+    if (_lodash2.default.isUndefined(value1) || _lodash2.default.isUndefined(value2)) {
+      if (_lodash2.default.isNull(value1)) {
+        value = value1;
+      } else {
+        value = value1 || value2;
+      }
+    } else if (_lodash2.default.isArray(value1) && _lodash2.default.isArray(value2)) {
+      value = _lodash2.default.concat(value1, value2);
+    } else if (_lodash2.default.isObject(value1) && _lodash2.default.isObject(value2)) {
+      value = Merge(value1, value2);
+    } else {
+      value = value1 || value2;
+    }
+
+    _return[key] = value;
+  }
+
+  return _return;
+}
 
 var index = void 0,
     _defaults = {
@@ -37,4 +73,4 @@ try {
   index = {};
 }
 
-exports.default = _lodash2.default.merge(index, _defaults);
+exports.default = Merge(index, _defaults);
